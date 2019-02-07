b6ab77c198e396a857e348e3419db4f899bb9526
Timothy Egenton <tegenton@ucvts.tec.nj.us>
2019-02-07 13:45:13 -0600

    start activity 2

--- Activity2/Deck.java
+++ Activity2/Deck.java
@@ -30,7 +30,13 @@ public class Deck {
 	 * @param values is an array containing all of the card point values.
 	 */
 	public Deck(String[] ranks, String[] suits, int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+	    for (String suit : suits) {
+	    	for (int i = 0; i < ranks.length; i++) {
+	    		cards.add(new Card(ranks[i], suit, values[i]));
+			}
+		}
+	    this.size = cards.size();
+	    shuffle();
 	}
 
 
@@ -39,7 +45,7 @@ public class Deck {
 	 * @return true if this deck is empty, false otherwise.
 	 */
 	public boolean isEmpty() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+	    return this.size == 0;
 	}
 
 	/**
@@ -47,7 +53,7 @@ public class Deck {
 	 * @return the number of undealt cards in this deck.
 	 */
 	public int size() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+	    return this.size;
 	}
 
 	/**
@@ -64,7 +70,8 @@ public class Deck {
 	 *         previously dealt.
 	 */
 	public Card deal() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return cards.get(--size);
+
 	}
 
 	/**
